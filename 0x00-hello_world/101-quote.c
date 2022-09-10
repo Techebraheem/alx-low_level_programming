767b1d8f772922ba417a02cd190e790d50bbb0e0
Techebraheem <ibrahimisyaka047@gmail.com>
2022-09-10 12:34:50 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			58);
 	return (1);
 }
 
