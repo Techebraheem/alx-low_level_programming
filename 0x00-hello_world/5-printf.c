f115a6640e71f403d6396f8a625ce33b2e5454cd
Techebraheem <ibrahimisyaka047@gmail.com>
2022-09-10 09:50:11 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * *main: The Entry point
+ * * main: The Entry point
  *  *
- *    *Return: Always 0 (Success)
- *     
+ *    * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
