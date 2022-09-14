6fdc49d09742326b4f9e3d2407cf815c082ca75a
Isyaka Ibrahim Maikudi <ibrahimisyaka047@gmail.com>
2022-09-15 00:17:49 +0100

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+unsigned int c;
+for (c = 0; c < sizeof(word); c++)
+{
+_putchar(word[c]);
+}
+_putchar('\n');
+return (0);
+}
